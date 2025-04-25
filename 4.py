eedebe49eabf7ee5928fa1606c48569cbd749b72
alfredo0688 <alfredonunez0688@gmail.com>
2025-04-25 20:29:37 -0300

    corregido ejercicio 4

--- 4.py
+++ 4.py
@@ -5,8 +5,11 @@ lista_numeros = [10,20,30]
 
 def calcularPromedio(longitud_lista, *lista):
     total_numeros = 0
+    contador = 0
     for numero in lista:
         total_numeros += numero
-        
+        total_numeros / longitud_lista if longitud_lista == contador else None
+        contador+= 1
+    print(f"El promedio de los números es : {total_numeros/longitud_lista}")
+calcularPromedio(len(lista_numeros),*lista_numeros )
 
-lista_numeros(len(lista_numeros),*lista_numeros )
